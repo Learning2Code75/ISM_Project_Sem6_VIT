658d6aba6df6bce2f5d7ae1c0d42c524f0d8a23a
Neel Choksi <neelchoksiwork@gmail.com>
2022-03-18 13:24:10 +0530

    modp-diffie-hellman node js code

---
+++ encryption-try1/modp-diffie-hellman/app.js
@@ -0,0 +1,60 @@
+const crypto = require("crypto");
+
+//client 1 :
+const alice = crypto.getDiffieHellman('modp15');
+
+//client 2 :
+const bob = crypto.getDiffieHellman('modp15');
+
+// console.log(alice.getPrime().toString('hex'));
+
+//client 1 :
+alice.generateKeys();
+
+//client 2 :
+bob.generateKeys();
+
+// client 1 :
+const alicePublicKey = alice.getPublicKey();
+const alicePrivateKey = alice.getPrivateKey();
+//client 2 :
+const bobPublicKey = bob.getPublicKey();
+const bobPrivateKey = bob.getPrivateKey();
+//from client 1, client 2 :
+const GlobalParameters = {
+  alicePublicKey,
+  bobPublicKey
+}
+
+//client 1:
+const AliceLocalParamenters ={
+  alicePublicKey,
+  alicePrivateKey,
+  sharedKey :"",
+}
+// console.log(AliceLocalParamenters.alicePrivateKey.toString('hex'));
+// console.log(AliceLocalParamenters.alicePublicKey.toString('hex'));
+
+
+//client2:
+const BobLocalParameters = {
+  bobPublicKey,
+  bobPrivateKey,
+  sharedKey :"",
+}
+// console.log(BobLocalParameters.bobPrivateKey.toString('hex'));
+// console.log(BobLocalParameters.bobPublicKey.toString('hex'));
+
+
+
+
+//client 1 :
+const aliceSharedSecret = alice.computeSecret(GlobalParameters.bobPublicKey,null,'hex');
+AliceLocalParamenters.sharedKey = aliceSharedSecret;
+
+//client 2 :
+const bobSharedSecret = bob.computeSecret(GlobalParameters.alicePublicKey,null,'hex');
+BobLocalParameters.sharedKey = bobSharedSecret;
+
+
+// console.log(AliceLocalParamenters.sharedKey == BobLocalParameters.sharedKey);
